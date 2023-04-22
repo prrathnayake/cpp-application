4dc2e86c2df71f731fc84f9eb0af5e55b32d4ed5
prrathnayake <pasanransika1@gmail.com>
2023-04-22 22:45:38 +0530

    add blockchain library

--- conanfile.py
+++ conanfile.py
@@ -13,6 +13,7 @@ class BaseRecipe(ConanFile):
         self.requires("app_kafka/1.0@pasan/testing")
         self.requires("app_rabbitmq/1.0@pasan/testing")
         self.requires("app_h3/1.0@pasan/testing")
+        self.requires("app_blockchain/1.0@pasan/testing")
         
     def build(self):
         cmake = CMake(self)
